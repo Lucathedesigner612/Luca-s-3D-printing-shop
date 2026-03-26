aaa234f4f20bd96102d39fa21c7cc7f2847921c3
Lucathedesigner612 <lucagalea612@gmail.com>
2026-03-26 21:24:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -46,9 +46,9 @@ if menu == "Browse Catalog":
     
     # Simple Product List
     products = [
-        {"name": "BB-gun", "price": 25, "img": "https://images.unsplash.com/photo-1595590424283-b8f17842773f?w=500"},
-        {"name": "6mm Cartridge", "price": 5, "img": "https://images.unsplash.com/photo-1584346133934-a3afd2a33c4c?w=500"},
-        {"name": "Custom Keyring", "price": 3, "img": "https://images.unsplash.com/photo-1584346133934-a3afd2a33c4c?w=300"}
+        {"name": "BB-gun", "price": 25, "img": "https://makerworld.bblmw.com/makerworld/model/US18955f0fc513e5/design/2024-01-03_201b2ae71df09.jpg?x-oss-process=image/resize,w_1000/format,webp"},
+        {"name": "6mm Cartridge", "price": 5, "img": "https://makerworld.bblmw.com/makerworld/model/US4eb0d6d10832a/design/2025-02-21_3d58e70697ae1.jpg"},
+        {"name": "Gatling gun", "price": 30, "img": "https://makerworld.bblmw.com/makerworld/model/US92c5fd98860546/design/2025-01-20_b0744fb62f5f2.gif?x-oss-process=image/resize,w_1000/format,webp"}
     ]
     
     colors = ["🔴 Matte Red", "⚫ Stealth Black", "⚪ Glossy White", "✨ Silk Gold", "🔵 Galaxy Blue"]
