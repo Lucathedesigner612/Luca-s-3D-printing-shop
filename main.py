e9ac0b184fd329e8c5e73c0bb1eb6ffe20ff8553
Lucathedesigner612 <lucagalea612@gmail.com>
2026-06-07 10:39:15 +0200

    Refactor main.py for improved structure and functionality

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ st.markdown("""
 # --- 2. PRODUCT DATABASE ---
 # Adjust the 'img' names to match the files you upload to GitHub
 products = [
-    {"name": "Japanese ONI Mask", "price": 25, "img": "https://ibb.co/Qv9TSByh"},
+    {"name": "Japanese ONI Mask", "price": 25, "img": "blob:https://gemini.google.com/ee3b456a-b4b5-4af3-92ff-2f96784f22e5"},
     {"name": "6mm Cartridge", "price": 5, "img": "cartridge.jpg"},
     {"name": "Gatling gun", "price": 30, "img": "gatling.jpg"},
     {"name": "BB-ammo", "price": 0.50, "img": "ammo.jpg"},
